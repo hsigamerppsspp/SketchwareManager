d6714710b689a3e2adc6a54b23491849cb1288f4
hsigamerppsspp <81436978+hsigamerppsspp@users.noreply.github.com>
2021-04-13 01:14:34 +0430

    Delete Dependencies.kt

--- buildSrc/src/main/kotlin/Dependencies.kt
+++
@@ -1,3 +0,0 @@
-const val kotlinSerializationJson =
-    "org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinSerializationVer"
-const val kotlinCoroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion"
